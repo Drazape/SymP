c0ba3d8647b9cbc3d4d47227b90670a7739d2143
Drazape <Drazape@outlook.com>
2025-11-12 11:18:22 +0530

    Completions: correct short-options

--- src/completion.fish
+++ src/completion.fish
@@ -6,9 +6,9 @@ complete --command='symp' --short-option='h' --long-option='help' 		--descriptio
 complete --command='symp' --short-option='v' --long-option='verbose'	 	--description='Show more information'
 
 
-complete --command='symp' --exclusive --short-option='b' 	--long-option='overwrites' 	--arguments='interactive' 	--description='Confirm interactively'
-complete --command='symp' --exclusive --short-option='b' 	--long-option='overwrites' 	--arguments='force'		--description='No confirmation'
-complete --command='symp' --exclusive --short-option='b' 	--long-option='overwrites' 					--description='Change behavior for overwrites' 
+complete --command='symp' --no-files --short-option='b' 	--long-option='blend' 						--description='Inherit access of the new parent'
+complete --command='symp' --no-files --short-option='b' 	--long-option='blend' 		--arguments='ownership'		--description='User & Group' 				--condition='providing-blend-value'
+complete --command='symp' --no-files --short-option='b' 	--long-option='blend' 		--arguments='permission'	--description='Discretionary Access Control' 		--condition='providing-blend-value'
 
 complete --command='symp' --exclusive --short-option='o' 	--long-option='occurrence' 	--arguments='common' 		--description='Present in the target'
 complete --command='symp' --exclusive --short-option='o' 	--long-option='occurrence' 	--arguments='unique' 		--description='Absent in the target (avoid overwrites)'
@@ -18,6 +18,6 @@ complete --command='symp' --exclusive --short-option='r' 	--long-option='resolut
 complete --command='symp' --exclusive --short-option='r' 	--long-option='resolution' 	--arguments='relative' 		--description='Point symlinks to relative path'
 complete --command='symp' --exclusive --short-option='r' 	--long-option='resolution' 					--description='Configure symlink resolution'
 
-complete --command='symp' --no-files --short-option='B' 	--long-option='blend' 						--description='Inherit access of the new parent'
-complete --command='symp' --no-files --short-option='B' 	--long-option='blend' 		--arguments='ownership'		--description='User & Group' 				--condition='providing-blend-value'
-complete --command='symp' --no-files --short-option='B' 	--long-option='blend' 		--arguments='permission'	--description='Discretionary Access Control' 		--condition='providing-blend-value'
+complete --command='symp' --exclusive --short-option='O' 	--long-option='overwrites' 	--arguments='interactive' 	--description='Confirm interactively'
+complete --command='symp' --exclusive --short-option='O' 	--long-option='overwrites' 	--arguments='force'		--description='No confirmation'
+complete --command='symp' --exclusive --short-option='O' 	--long-option='overwrites' 					--description='Change behavior for overwrites' 
