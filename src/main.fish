e5e6146cd438d1807506680c4ee022269fc23241
Drazape <Drazape@outlook.com>
2025-11-13 12:32:52 +0530

    only check for flag

--- src/main.fish
+++ src/main.fish
@@ -70,8 +70,6 @@ if set -ql {$_flag_resolution}
 	if ! contains {$_flag_resolution} 'a' 'absolute'
 		set --global --export relative_resolution
 	end
-else
-	set --global --export relative_resolution
 end
 
 ##### Print Functions
