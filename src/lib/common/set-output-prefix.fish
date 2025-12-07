4217dab51de39de68daa6013c8df7c71aa1a7893
Drazape <Drazape@outlook.com>
2025-12-07 14:05:24 +0530

    refactor: if not to be done, return early

--- src/lib/common/set-output-prefix.fish
+++ src/lib/common/set-output-prefix.fish
@@ -1,9 +1,9 @@
 function _symp_common_set-output-prefix --no-scope-shadowing --description='Set the output prefix for the current function'
+	set -qg SYMP_LIST_FUNCTIONS || return 1
+
 	argparse --m{in,ax}=1 'r/reset&' -- {$argv}
 	set --erase --local _flag_r # Unused
 	set -ql _flag_reset || set --function -- append '--append'
 
-	if set -qg SYMP_LIST_FUNCTIONS
-		set --global {$append} output_prefix (set_color --dim)(string split --right --max=1 --fields=2 -- '_' {$argv})':'(set_color normal) # Append the Output-prefix with the current function name
-	end
+	set --global {$append} output_prefix (set_color --dim)(string split --right --max=1 --fields=2 -- '_' {$argv})':'(set_color normal) # Append the Output-prefix with the current function name
 end
