6367c0897d88895f6a47887e065fa68cd14d2c0d
Drazape <Drazape@outlook.com>
2025-12-25 10:40:51 +0530

    Respect rootdir switch for license

--- install.fish
+++ install.fish
@@ -185,4 +185,4 @@ if ! set -ql _flag_symlink
 end
 
 # Add license on vendor installation
-set -ql _flag_vendor || true && install -D --mode=644 {$VERBOSE} -- ../LICENSE /usr/share/licenses/{$executable_name}/LICENSE
+set -ql _flag_vendor || true && install -D --mode=644 {$VERBOSE} -- ../LICENSE "$_flag_rootdir"/usr/share/licenses/{$executable_name}/LICENSE
