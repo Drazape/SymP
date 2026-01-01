96149479c33d31a3fa2d5281829f9057931ab4a8
Drazape <Drazape@outlook.com>
2026-01-01 11:34:54 +0530

    Completion: complete blend arguments in short form

--- src/completion.fish
+++ src/completion.fish
@@ -1,31 +1,28 @@
 function providing-blend-value
-	string match --quiet --regex -- '^--blend=\w*$' "$(commandline --current-process --current-token)"
+    string match --quiet --regex -- '^(--blend=|-b)\w*$' "$(commandline --current-process --current-token)"
 end
 
 begin # Directories
-	set --local directories '(__fish_complete_directories)'
-	complete --command='symp' --no-files --arguments={$directories}
+    set --local directories '(__fish_complete_directories)'
+    complete --command='symp' --no-files --arguments={$directories}
 end
 
+complete --command='symp' --short-option='h' --long-option='help' --description='Print help' --condition='! providing-blend-value'
+complete --command='symp' --short-option='v' --long-option='verbose' --description='Show more information' --condition='! providing-blend-value'
 
+complete --command='symp' --no-files --short-option='b' --long-option='blend' --description='Inherit access of the new parent' --condition='! providing-blend-value'
+complete --command='symp' --no-files --short-option='b' --long-option='blend' --arguments='ownership' --description='User & Group' --condition='providing-blend-value'
+complete --command='symp' --no-files --short-option='b' --long-option='blend' --arguments='permission' --description='Discretionary Access Control' --condition='providing-blend-value'
 
-complete --command='symp' --short-option='h' --long-option='help' 		--description='Print help'
-complete --command='symp' --short-option='v' --long-option='verbose'	 	--description='Show more information'
+complete --command='symp' --exclusive --short-option='o' --long-option='occurrence' --arguments='common' --description='Present in the target' --condition='! providing-blend-value'
+complete --command='symp' --exclusive --short-option='o' --long-option='occurrence' --arguments='unique' --description='Absent in the target (avoid overwrites)' --condition='! providing-blend-value'
+complete --command='symp' --exclusive --short-option='o' --long-option='occurrence' --description='Filter files based on their occurrences' --condition='! providing-blend-value'
 
+complete --command='symp' --exclusive --short-option='r' --long-option='resolution' --arguments='absolute' --description='Point symlinks to absolute path' --condition='! providing-blend-value'
+complete --command='symp' --exclusive --short-option='r' --long-option='resolution' --arguments='relative' --description='Point symlinks to relative path' --condition='! providing-blend-value'
+complete --command='symp' --exclusive --short-option='r' --long-option='resolution' --description='Configure symlink resolution' --condition='! providing-blend-value'
 
-complete --command='symp' --no-files --short-option='b' 	--long-option='blend' 						--description='Inherit access of the new parent'
-complete --command='symp' --no-files --short-option='b' 	--long-option='blend' 		--arguments='ownership'		--description='User & Group' 				--condition='providing-blend-value'
-complete --command='symp' --no-files --short-option='b' 	--long-option='blend' 		--arguments='permission'	--description='Discretionary Access Control' 		--condition='providing-blend-value'
-
-complete --command='symp' --exclusive --short-option='o' 	--long-option='occurrence' 	--arguments='common' 		--description='Present in the target'
-complete --command='symp' --exclusive --short-option='o' 	--long-option='occurrence' 	--arguments='unique' 		--description='Absent in the target (avoid overwrites)'
-complete --command='symp' --exclusive --short-option='o' 	--long-option='occurrence' 					--description='Filter files based on their occurrences'
-
-complete --command='symp' --exclusive --short-option='r' 	--long-option='resolution' 	--arguments='absolute' 		--description='Point symlinks to absolute path'
-complete --command='symp' --exclusive --short-option='r' 	--long-option='resolution' 	--arguments='relative' 		--description='Point symlinks to relative path'
-complete --command='symp' --exclusive --short-option='r' 	--long-option='resolution' 					--description='Configure symlink resolution'
-
-complete --command='symp' --exclusive --short-option='O' 	--long-option='overwrites' 	--arguments='backup'	 	--description='Backup before overwriting'
-complete --command='symp' --exclusive --short-option='O' 	--long-option='overwrites' 	--arguments='interactive' 	--description='Confirm interactively'
-complete --command='symp' --exclusive --short-option='O' 	--long-option='overwrites' 	--arguments='force'		--description='No confirmation'
-complete --command='symp' --exclusive --short-option='O' 	--long-option='overwrites' 					--description='Change behavior for overwrites' 
+complete --command='symp' --exclusive --short-option='O' --long-option='overwrites' --arguments='backup' --description='Backup before overwriting' --condition='! providing-blend-value'
+complete --command='symp' --exclusive --short-option='O' --long-option='overwrites' --arguments='interactive' --description='Confirm interactively' --condition='! providing-blend-value'
+complete --command='symp' --exclusive --short-option='O' --long-option='overwrites' --arguments='force' --description='No confirmation' --condition='! providing-blend-value'
+complete --command='symp' --exclusive --short-option='O' --long-option='overwrites' --description='Change behavior for overwrites' --condition='! providing-blend-value'
