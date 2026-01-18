0c5e99d390c10e64e3611953c5241a8fe9525b29
Drazape <Drazape@outlook.com>
2026-01-18 12:06:18 +0530

    Recurse: Use output prefix

--- src/lib/operate/case/recursive/children/recurse.fish
+++ src/lib/operate/case/recursive/children/recurse.fish
@@ -1,8 +1,8 @@
 function _symp_operate_case_recursive_children_recurse --description='Re-operate on a new set of directories'
-	_"$program_name"_common_set-output-prefix (status current-function)
+    _"$program_name"_common_set-output-prefix (status current-function)
 
-	set -qg SYMP_VERBOSE && echo 'Recursing:'\t'Source: '(set_color --bold){$argv[1]}(set_color normal)' target: '(set_color --bold){$argv[2]}(set_color normal) # Verbosity announcement
+    set -qg SYMP_VERBOSE && echo {$output_prefix} 'Recursing:'\t'Source: '(set_color --bold){$argv[1]}(set_color normal)' target: '(set_color --bold){$argv[2]}(set_color normal) # Verbosity announcement
 
-	# Main
-	source_dir={$argv[1]} target_path={$argv[2]} "$operate_function"
+    # Main
+    source_dir={$argv[1]} target_path={$argv[2]} "$operate_function"
 end
