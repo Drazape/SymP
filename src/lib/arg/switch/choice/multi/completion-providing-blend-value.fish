808180170faab0495c1ce6e1a76c01f1aaa19659
Drazape <Drazape@outlook.com>
2026-01-23 18:47:50 +0530

    Update providing blend value function name

--- src/lib/arg/switch/choice/multi/completion-providing-blend-value.fish
+++ src/lib/arg/switch/choice/multi/completion-providing-blend-value.fish
@@ -1,3 +1,3 @@
-function _symp_completion_providing-blend-value --description='Check if a value is given to the switch: blend'
-	string match --quiet --regex -- '^(--blend=|-b)\w*$' "$(commandline --current-process --current-token)"
+function _symp_arg_switch_choice_multi_completion-providing-blend-value --description='Check if a value is given to the switch: blend'
+    string match --quiet --regex -- '^(--blend=|-b)\w*$' "$(commandline --current-process --current-token)"
 end
