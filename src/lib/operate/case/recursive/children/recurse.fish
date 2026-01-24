bc4bfe98d31c87a1eb8ef0f30c317ecd41a855c6
Drazape <Drazape@outlook.com>
2026-01-24 13:00:16 +0530

    recurse in background

--- src/lib/operate/case/recursive/children/recurse.fish
+++ src/lib/operate/case/recursive/children/recurse.fish
@@ -4,5 +4,5 @@ function _symp_operate_case_recursive_children_recurse --description='Re-operate
     set -qg SYMP_VERBOSE && echo {$output_prefix} 'Recursing:'\t'Source: '(set_color --bold){$argv[1]}(set_color normal)' target: '(set_color --bold){$argv[2]}(set_color normal) # Verbosity announcement
 
     # Main
-    source_dir={$argv[1]} target_path={$argv[2]} "$operate_function"
+    source_dir={$argv[1]} target_path={$argv[2]} "$operate_function" &
 end
