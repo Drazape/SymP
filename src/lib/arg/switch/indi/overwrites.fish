1a2e08e36997d70f1239fe29b5f398313c7f4319
Drazape <Drazape@outlook.com>
2026-02-01 12:32:27 +0530

    Use `argument-names` for `function`s

--- src/lib/arg/switch/indi/overwrites.fish
+++ src/lib/arg/switch/indi/overwrites.fish
@@ -1,5 +1,7 @@
-function _symp_arg_switch_indi_overwrites --description='Parse switch: overwrites'
-	# If the argv is not in the short form, add an extra `-` to it
-	test (count (string split -- \0 {$argv})) -ne 1 && set -- argv '-'{$argv}
-	set --global -- overwrites '-'{$argv}
+function _symp_arg_switch_indi_overwrites --description='Parse switch: overwrites' --argument-names='switch_val'
+	set --erase --local argv # Unused
+
+	# If the `switch_val` is not in the short form, add an extra `-` to it
+	test (count (string split -- \0 {$switch_val})) -ne 1 && set -- switch_val '-'{$switch_val}
+	set --global -- overwrites '-'{$switch_val}
 end
--- src/lib/operate/case/recursive/children/overwrite.fish
+++ src/lib/operate/case/recursive/children/overwrite.fish
@@ -1,8 +1,6 @@
-function _symp_operate_case_recursive_children_overwrite --description='Forcefully remove the target item (if it exists) and overwrite it with the source item'
+function _symp_operate_case_recursive_children_overwrite --argument-names={source,target}_item --description='Forcefully remove the target item (if it exists) and overwrite it with the source item'
 	_"$program_name"_common_set-output-prefix (status current-function)
-
-	set --local -- source_item {$argv[1]}
-	set --local -- target_item {$argv[2]}
+	set --erase --local argv # Unused (Argument names)
 
 	test -e {$target_item} && "$operate_function"_file_remove -- "$target_item" # Remove target item if it exists
 	"$operate_function"_file_symlink -- "$source_item" "$target_item"
